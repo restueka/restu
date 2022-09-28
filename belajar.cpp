a5b05194976cf830ff2a57d5e924c9e09279d890
root <root@DESKTOP-VLGHPPB.localdomain>
2022-09-28 02:43:20 -0700

    first commit

---
+++ belajar.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+sysytem('abc');
+    int sequential[3] = {16,25,32,47,56}
+    int cari;
+    int flag = 0;
+    cout<< "Masukkan bilangan yang ingin dicari = ";
+    cin>> cari;
+    for (int i = 0, i<3, i++)
+    {
+        if (sequential[i]==cari) flag=1;
+    }
+
+
+
+
+   return 0;
+}
